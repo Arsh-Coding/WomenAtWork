9c1b7003a8f85c358fd9c2e2af71485c77d9c7a1
komalghaisdna <komal.ghai@sdnatech.com>
2025-01-15 14:02:23 +0530

    made responsive

--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from 'react';
+import './Navbar.css';
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-     <div className='navbar'>
+      <div className='navbar'>
         <div className='logo'>
-            <img src="./logo.png" alt="" />
+          <img src='./logo.png' alt='Logo' />
         </div>
-        <div className='navLinks'>
-            <div className='Links'>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Jobs</li>
-                    <li>WOW</li>
-                    <li>Contact</li>
-                </ul>
-                </div>
-                <button>SignIn/Register</button>
+        <div className={`navLinks ${menuOpen ? 'active' : ''}`}>
+          <div className='Links'>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Jobs</li>
+              <li>WOW</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <button>SignIn/Register</button>
         </div>
-     </div>
+        <div className='hamburger' onClick={toggleMenu}>
+          &#9776; {/* Hamburger Icon */}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
