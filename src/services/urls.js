0ddeb954c4875f3aff23724ec576191a7ba228d4
Arshpreet Singh <arshpreet.singh@sdnatech.com>
2025-05-26 18:01:18 +0530

    chore(endpoint) changed endpoint for backend

--- src/services/urls.js
+++ src/services/urls.js
@@ -2,8 +2,8 @@
 
 import { createOrUpdateCompany } from "./slices/companySlice";
 
-export const apiEndpoint = "http://localhost:3000/";
-// const apiEndpoint = "https://woman-at-workforce-backend.onrender.com/";
+// export const apiEndpoint = "http://localhost:3000/";
+const apiEndpoint = "https://woman-at-workforce-backend.onrender.com/";
 
 export const URLS = {
   jobs: (jobId) => `${apiEndpoint}jobs/${jobId}`,
